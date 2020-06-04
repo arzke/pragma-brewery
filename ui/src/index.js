71a50ff355e1ba49bd64c28df5d559034a351545
Bruno Arsene <arsene.bruno@gmail.com>
2020-06-04 17:13:42 +0930

    Create basic page

---
+++ ui/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../static/app.scss'
+
+const App = () => (
+  <div>
+    <div className='page' />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
