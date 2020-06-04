f3b5aa6d366e8b6af0af82f3960aa5a6903c63a2
Bruno Arsene <arsene.bruno@gmail.com>
2020-06-04 17:44:11 +0930

    Add apollo-client to ApolloProvider

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,21 @@
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
 import '../static/app.scss'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
 const App = () => (
-  <div>
-    <Header />
-    <div className='page' />
-  </div>
+  <ApolloProvider client={client}>
+    <div>
+      <Header />
+      <div className='page'></div>
+    </div>
+  </ApolloProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
