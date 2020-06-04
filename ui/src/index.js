e3b3bbc6e7b8e70c20c1ac8979b806c70ddb8078
Bruno Arsene <arsene.bruno@gmail.com>
2020-06-04 17:17:38 +0930

    Add Header to App

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/Header'
 import '../static/app.scss'
 
 const App = () => (
   <div>
+    <Header />
     <div className='page' />
   </div>
 )
