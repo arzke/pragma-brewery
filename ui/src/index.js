6e5bba652513e93058b383d3eb4042bd21268dd7
Bruno Arsene <arsene.bruno@gmail.com>
2020-06-08 10:40:21 +0930

    Render the ContainersData component in the App

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
+import ContainersData from './components/ContainersData'
 import '../static/app.scss'
 
 const client = new ApolloClient({
@@ -13,7 +14,7 @@ const App = () => (
   <ApolloProvider client={client}>
     <div>
       <Header />
-      <div className='page'></div>
+      <ContainersData />
     </div>
   </ApolloProvider>
 )
