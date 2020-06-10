747cc8d30340ba7f3d913fdb98b8a8ab177112e4
Bruno Arsene <arsene.bruno@gmail.com>
2020-06-10 22:19:30 +0930

    Send notification from Temperature when container temperature is too cold or too hot

--- ui/src/components/temperature/Temperature.js
+++ ui/src/components/temperature/Temperature.js
@@ -1,4 +1,38 @@
 import React from 'react'
+import NotificationsContext from '../../context/NotificationsContext'
+import getNotifier from '../../helpers/notifications'
+
+const temperatureNotificationFactory = (containerId, temperature, minTemperature, maxTemperature) => {
+  if (temperature > maxTemperature) {
+    return {
+      type: 'CONTAINER_IS_TOO_HOT',
+      id: containerId
+    }
+  }
+
+  if (temperature < minTemperature) {
+    return {
+      type: 'CONTAINER_IS_TOO_COLD',
+      id: containerId
+    }
+  }
+
+  return null
+}
+
+const notifyOnTemperatureOutOfRange = (containerId, temperature, minTemperature, maxTemperature, notifier) => {
+  return () => {
+    if (temperature) {
+      const notification = temperatureNotificationFactory(containerId, temperature, minTemperature, maxTemperature)
+
+      if (notification) {
+        notifier.notify(notification)
+      } else {
+        notifier.deleteNotifications(containerId)
+      }
+    }
+  }
+}
 
 const getClassName = (temperature, minTemperature, maxTemperature) => {
   if (temperature < minTemperature) {
@@ -13,6 +47,14 @@ const getClassName = (temperature, minTemperature, maxTemperature) => {
 }
 
 const Temperature = ({ containerId, temperature, minTemperature, maxTemperature }) => {
+  const { notifications, setNotifications } = React.useContext(NotificationsContext)
+  const notifier = getNotifier(notifications, setNotifications)
+
+  React.useEffect(
+    notifyOnTemperatureOutOfRange(containerId, temperature, minTemperature, maxTemperature, notifier),
+    [temperature]
+  )
+
   return <span className={getClassName(temperature, minTemperature, maxTemperature)}>{temperature}°C</span>
 }
 
--- ui/src/components/temperature/__tests__/Temperature.test.js
+++ ui/src/components/temperature/__tests__/Temperature.test.js
@@ -1,34 +1,106 @@
 import React from 'react'
-import { describe, expect, test } from '@jest/globals'
+import { beforeEach, describe, expect, test, jest } from '@jest/globals'
 import { shallow } from 'enzyme'
 import Temperature from '../Temperature'
+import getNotifier from '../../../helpers/notifications'
+
+jest.mock('../../../helpers/notifications')
+
+let temperature
+
+let notifications
+let setNotificationsMock
+
+let notifyMock
+let deleteNotificationsMock
+
+beforeEach(() => {
+  notifications = []
+  setNotificationsMock = jest.fn()
+
+  const useContextMock = jest.fn(() => ({
+    notifications,
+    setNotifications: setNotificationsMock
+  }))
+
+  jest.spyOn(React, 'useContext')
+    .mockImplementation(useContextMock)
+
+  jest.spyOn(React, 'useEffect')
+    .mockImplementation(f => f())
+})
+
+beforeEach(() => {
+  notifyMock = jest.fn()
+  deleteNotificationsMock = jest.fn()
+
+  getNotifier.mockReturnValue({
+    notify: notifyMock,
+    deleteNotifications: deleteNotificationsMock
+  })
+})
 
 test('it displays the temperature', () => {
-  const temperature = shallow(<Temperature temperature={4} minTemperature={5} maxTemperature={7} />)
+  const temperature = shallow(<Temperature containerId='42' temperature={4} minTemperature={5} maxTemperature={7} />)
 
   expect(temperature.find('span')).toHaveText('4°C')
 })
 
 describe('when temperature is lower than minimum temperature', () => {
-  test('it adds the "too-cold" class to the temperature', () => {
-    const temperature = shallow(<Temperature temperature={4} minTemperature={5} maxTemperature={7} />)
+  beforeEach(() => {
+    temperature = shallow(<Temperature containerId='42' temperature={4} minTemperature={5} maxTemperature={7} />)
+  })
 
+  test('it adds the "too-cold" class to the temperature', () => {
     expect(temperature).toContainMatchingElement('.too-cold')
+  })
+
+  test('it calls the notify function with a container too cold notification', () => {
+    expect(notifyMock).toHaveBeenCalledWith({
+      type: 'CONTAINER_IS_TOO_COLD',
+      id: '42'
+    })
+  })
+  test('it does not call the deleteNotifications function', () => {
+    expect(deleteNotificationsMock).not.toHaveBeenCalled()
   })
 })
 
 describe('when temperature is higher than maximum temperature', () => {
-  test('it adds the "too-hot" class to the temperature', () => {
-    const temperature = shallow(<Temperature temperature={8} minTemperature={5} maxTemperature={7} />)
+  beforeEach(() => {
+    temperature = shallow(<Temperature containerId='42' temperature={8} minTemperature={5} maxTemperature={7} />)
+  })
 
+  test('it adds the "too-hot" class to the temperature', () => {
     expect(temperature).toContainMatchingElement('.too-hot')
+  })
+
+  test('it calls the notify function with a container too hot notification', () => {
+    expect(notifyMock).toHaveBeenCalledWith({
+      type: 'CONTAINER_IS_TOO_HOT',
+      id: '42'
+    })
+  })
+
+  test('it does not call the deleteNotifications function', () => {
+    expect(deleteNotificationsMock).not.toHaveBeenCalled()
   })
 })
 
-describe('when temperature is between the minimum and the maximum temperatures', () => {
-  test('it adds the "in-range" class to the temperature', () => {
-    const temperature = shallow(<Temperature temperature={6} minTemperature={5} maxTemperature={7} />)
+describe('when temperature is between the minimum and the maximum temperature', () => {
+  beforeEach(() => {
+    temperature = shallow(<Temperature containerId='42' temperature={6} minTemperature={5} maxTemperature={7} />)
+  })
 
+  test('it adds the "in-range" class to the temperature', () => {
     expect(temperature).toContainMatchingElement('.in-range')
+  })
+
+  test('it calls the notify function with a container too hot notification', () => {
+    expect(notifyMock).not.toHaveBeenCalled()
+  })
+
+  test('it does not call the deleteNotifications function', () => {
+    expect(deleteNotificationsMock).toHaveBeenCalledWith('42')
   })
 })
